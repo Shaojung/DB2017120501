f4c4299ceaf797fedc5d989d6a4c9e6aef651df8
Shaojung <shaojung@gmail.com>
2017-12-05 19:39:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/db2017120501/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.student.db2017120501;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+             
+
+            }
+        }.start();
+    }
+}
