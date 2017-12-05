a7f602db3e5a28f2a4e38de3d8c80227e65a40c3
Shaojung <shaojung@gmail.com>
2017-12-05 19:48:10 +0800

    read internet data

--- app/src/main/java/com/example/student/db2017120501/MainActivity.java
+++ app/src/main/java/com/example/student/db2017120501/MainActivity.java
@@ -2,6 +2,16 @@ package com.example.student.db2017120501;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +24,29 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 super.run();
-             
+                String str_url = "http://www.google.com.tw";
+                URL url = null;
+                try {
+                    url = new URL(str_url);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.connect();
+                    InputStream inputStream = conn.getInputStream();
+                    InputStreamReader reader = new InputStreamReader(inputStream);
+                    BufferedReader br = new BufferedReader(reader);
+                    String str = br.readLine();
+                    Log.d("NET", str);
+
+
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
 
             }
         }.start();
